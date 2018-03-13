40ddc203ca4c6756c10ca11a915fa97198523f75
Tiago Henrique Angioletti <angioletti@localhost.localdomain>
2018-03-13 08:28:31 -0300

    Lista 3 Exer 3

--- src/Lista3/Exer3/Buscador.java
+++ src/Lista3/Exer3/Buscador.java
@@ -1,4 +1,5 @@
 package Lista3.Exer3;
+
 import java.io.File;
 import java.io.FilenameFilter;
 
@@ -7,43 +8,33 @@ import javax.swing.JOptionPane;
 
 //aqui é feito o codigo que roda em cada thread
 public class Buscador extends Thread {
-   
-	public static int numExec = 0;
-    
+
+    public static int numExec = 0;
+
     private File arquivo1;
     private String nome1;
     private String ext1;
-    private boolean nome2;
-    private boolean ext2;
-    FileChooser fc = new FileChooser();
-    
+
     //metodo responsavel por setar os valores pra classe
-    public Buscador(File arquivo1, String nome1, String ext1, boolean nome2, boolean ext2) {
-        
-    	numExec++;
+    public Buscador(File arquivo1, String nome1, String ext1) {
+
+        numExec++;
         this.arquivo1 = arquivo1;
         this.nome1 = nome1;
-        this.nome2 = nome2;
         this.ext1 = ext1;
-        this.ext2 = ext2;
-        
+
     }
-    
+
     //aqui inicia a thread
     @Override
     public void run() {
-        search(arquivo1, nome1, ext1, nome2, ext2);
+        search(arquivo1, nome1, ext1);
         numExec--;
     }
 
     //aqui esta o metodo que a thread reproduz
-    public void search(File arquivo1, String nome1, String ext1, boolean nome2, boolean ext2) {
+    public void search(File arquivo, String nome, String ext) {
 
-        File arquivo = arquivo1;
-        String nome = nome1;
-        String ext = ext1;
-        boolean nomen = nome2;
-        boolean extn = ext2;
 
         if (arquivo.exists()) {
 
@@ -57,19 +48,18 @@ public class Buscador extends Thread {
                 System.out.println(contarquivos + " - " + file);
                 contarquivos++;
 
-                
                 //esta serie de ifs � dedicada a encerrar o programa quando ele obter o resultado
-                if (file.startsWith(nome) && file.endsWith(ext) && !nomen && !extn) {
+                if (file.startsWith(nome) && file.endsWith(ext)) {
 
                     System.out.println("\n\nUm arquivo conforme especificado foi encontrado na pasta:\n\n" + arquivo + "/");
-                    System.exit(0);  
-                    
+                    System.exit(0);
+
                 } else {
-                    if (file.startsWith(nome) && extn) {
+                    if (file.startsWith(nome)) {
                         System.out.println("\n\nUm arquivo conforme especificado foi encontrado na pasta:\n\n" + arquivo + "/");
                         System.exit(0);
                     } else {
-                        if (file.endsWith(ext) && nomen) {
+                        if (file.endsWith(ext)) {
                             System.out.println("\n\nUm arquivo conforme especificado foi encontrado na pasta:\n\n" + arquivo + "/");
                             System.exit(0);
                         }
@@ -77,7 +67,7 @@ public class Buscador extends Thread {
                 }
 
             }
-            
+
             //aqui � analizado se uma subpasta foi encontrada, ent�o por meio de recursividade
             // � criada uma nova thread dentro da thread pai
             for (String file : arquivos) {
@@ -86,14 +76,13 @@ public class Buscador extends Thread {
                     System.out.println("\nPasta encontrada: " + file + "\n");
 
                     File arquivo2 = new File(arquivo + "/" + file);
-       
+
                     System.out.println("Nova thread para " + arquivo2);
-                    Buscador w = new Buscador(arquivo2, nome, ext, nomen, extn);
+                    Buscador w = new Buscador(arquivo2, nome, ext);
                     w.start();
                 }
             }
 
-         
         }
     }
 }
